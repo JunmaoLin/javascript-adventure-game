94c35fb3d960231835fcf5aaee0851f8bb83b2dc
JunmaoLin <95445468+JunmaoLin@users.noreply.github.com>
2021-12-10 15:38:54 -0500

    added objects with text

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log("script is loading");
+
+let firstStage = {
+    id: 1,
+    story: "Juan got lost in the forest, please help Juan find his way out now!",
+    options: ["start heading north", "start heading south", "find a path", "follow the river"],
+}
+
+let secondStage = {
+    id: 2,
+    story: "",
+    options: [],
+}
+
+let thirdStage = {
+    id: 3,
+    story: "",
+    options: [],
+}
+
+let fourthStage = {
+    id: 4,
+    story: "",
+    options: [],
+}
