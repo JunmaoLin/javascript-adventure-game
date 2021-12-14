dfc3a0d4d8f4dc276a70ed8ce471857c2f89ca99
JunmaoLin <95445468+JunmaoLin@users.noreply.github.com>
2021-12-14 16:02:33 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,65 @@
 console.log("script is loading");
 
-let adventureDescriptionText = document.querySelector(".adventure-description");
-let optionButtons = document.getElementsByClassName("button");
+// select the container of the game description
+const descriptionText = document.querySelector(".adventure-description");
+const optionButtons = document.getElementsByClassName("button");
 
-function updateAdventureText() {
-  adventureDescriptionText.innerText = textAdventure[0].text;
+// function that starts the game
+function startAdventure() {
+  console.log("Adventure has started");
+  showAdventureOptions(1);
 }
 
-function updateOptions() {
-  let i = 0;
-  for(i; i < optionButtons.length; i++) {
-    console.log(optionButtons[i]);
-    optionButtons[i].innerText = textAdventure[0].options[i];
+// create a function that shows the description
+function showAdventureOptions(textAdventureID) {
+  const newDescriptionText = textAdventure.find(gameNode => gameNode.id === textAdventureID);
+  descriptionText.innerText = newDescriptionText.description;
+  // iterate through the buttons and insert the option text in each button
+  for(let i = 0; i < newDescriptionText.options.length; i++) {
+    //console.log(newDescriptionText.options[i].buttontext);
+    optionButtons[i].innerText = newDescriptionText.options[i].buttontext;
   }
 }
 
+// function that tracks options and passes the id to the showAdventureOptions function
+
+
+// optional: function that shows end of the game
+
+
+// create and object that holds the text for our game and options and also tracks state
 const textAdventure = [
   {
     id: 1,
-    text: 'This is the adventure of Juan, who got lost in the forest when looking for a bird to photograph. Juan needs your help to find his way out. Choose one of the options below',
-    options: ["Head south and find the river" , "Go back to the bottom of the hill" ]
+    description: 'This is the adventure of Juan, who got lost in the forest when looking for a bird to photograph. Juan needs your help to find his way out. Choose one of the options below',
+    options: [
+      {
+        buttontext: "Head back to where you came from",
+        setState: { headback: true },
+        nexttext: 2
+      },
+      {
+        buttontext: "Look up at the sky and head towards the sun",
+        nextText: 2       
+      }
+    ]
   },
-]
+  {
+    id: 2,
+    description: 'great, Juan was able to find his way back, but now he reliazes that its about to rain, what should Juan do now?',
+    options: [
+      {
+        buttontext: "Look for shelter",
+        setState: { headback: true },
+        nexttext: 3
+      },
+      {
+        buttontext: "Continue heading back, and risk getting lost again",
+        nextText: 2       
+      }
+    ]
+  },
+  
+];
 
-updateAdventureText();
-updateOptions();
+startAdventure();
